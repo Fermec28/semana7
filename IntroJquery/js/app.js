84bf35d9418ad0f6c2e452c8277806c4be46822a
Jose Medina <fermed28@gmail.com>
2018-11-22 00:23:19 -0500

    Introduction to jquery

---
+++ IntroJquery/js/app.js
@@ -0,0 +1,46 @@
+// ejemplo juego simple jquery
+//https://fermec28.github.io/Fershoot/
+
+/**
+ * Conectar app.js
+ */
+//console.log("conectado")  // El archivo esta correctamente referenciado 
+
+/**
+ * Manipular Dom con vanilla Js
+ */
+
+//let body = document.querySelector('*');
+//console.log(body)
+
+//body.classList.add('red')
+//body.classList.remove('red')
+
+//body.classList.add('blue')
+//body.classList.remove('blue')
+
+//body.classList.add('yellow')
+//body.classList.remove('yellow')
+
+/**
+ * conectar Juery
+ */
+//console.log($)
+
+/**
+ * Manipular con Jquery
+ */
+
+//$('*').addClass('blue')
+//$('*').removeClass('blue')
+
+//$('*').addClass('yellow')
+//$('*').removeClass('yellow')
+
+//$('*').addClass('red')
+//$('*').removeClass('red')
+
+/**
+ * Crear una funcion que al ejecutarla me cambie el color de la pagina.
+ * imprimir el color cambiado
+ */
